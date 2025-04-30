b3a8164b45bee8bf775dc75833ffc8017f814edf
Harm Putman <h.putman@am-impact.nl>
2025-04-30 14:32:15 +0200

    ✨ convert textnode to htmlnode
    CH2-L6

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -22,5 +22,27 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Lorem ipsum", TextType.BOLD, "https://www.high-five.dev")
         self.assertNotEqual(node.url, None)
 
+    def test_text_to_html(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = node.to_html_node()
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image_to_html(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = node.to_html_node()
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold_to_html(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = node.to_html_node()
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
 if __name__ == "__main__":
     unittest.main()

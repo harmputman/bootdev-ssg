bc065fb6e912be5c88e03c057087b88df5f73653
Harm Putman <h.putman@am-impact.nl>
2025-05-07 16:10:23 +0200

    ✨ extract links
    CH3-L4

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 from textnode import TextType, TextNode
-
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -21,3 +21,8 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]+)\]\(([^\(\)]+)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]+)\)", text)

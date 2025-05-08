a23df87e767cb5059fff329710c062e43c7a16a8
Harm Putman <h.putman@am-impact.nl>
2025-05-08 17:31:50 +0200

    ✨ split images and links
    CH3-L5

--- src/utils.py
+++ src/utils.py
@@ -21,8 +21,40 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    return split_nodes_regex(old_nodes, extract_markdown_images, "![TEXT](URL)", TextType.IMAGE)
+
+def split_nodes_link(old_nodes):
+    return split_nodes_regex(old_nodes, extract_markdown_links, "[TEXT](URL)", TextType.LINK)
+
+def split_nodes_regex(old_nodes, method, pattern, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        items = method(original_text)
+        if (len(items) == 0):
+            new_nodes.append(old_node)
+            continue
+        for (text, url) in items:
+            new_pattern = pattern.replace('TEXT', text)
+            new_pattern = new_pattern.replace('URL', url)
+            sections = original_text.split(new_pattern, 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(text, text_type, url))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]+)\]\(([^\(\)]+)\)", text)
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]+)\)", text)
+

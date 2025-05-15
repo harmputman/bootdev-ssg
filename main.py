598e1e933f56669750750b90130b2699b8b3f814
Harm Putman <h.putman@am-impact.nl>
2025-05-15 23:12:13 +0200

    ♻️ minor refactor for sys.argv usage

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
-basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
